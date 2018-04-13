3f91eda2eeb0a9b89903e57a593c9203a56f17e6
Joanna Joseph <elainefirefly@gmail.com>
2018-04-13 14:09:13 -0700

    Make the Xylophone produce sound when a key is touched

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,33 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController{
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        let fileName = "note" + String(sender.tag)
+        let ext = ".wav"
+        playSound(filename: fileName, ext: ext)
     }
     
-  
+    func playSound(filename: String, ext: String) {
+        if let soundURL = Bundle.main.url(forResource: filename, withExtension: ext) {
+            
+            var soundId: SystemSoundID = 0
+        
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &soundId)
+            AudioServicesAddSystemSoundCompletion(soundId, nil, nil, { (soundId, clientData) -> Void in
+                AudioServicesDisposeSystemSoundID(soundId)
+            }, nil)
+        
+            AudioServicesPlaySystemSound(soundId)
+        }
+    }
 
 }
 
